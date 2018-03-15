5105ec001fbeea5d30b2c17724ef73dc28127c57
gusch <guscha.vladislav1997@yandex.ru>
2018-03-15 15:54:48 +0300

    use annotation Logger

--- src/main/java/com/tsystems/javaschoolshop/aop/Logging.java
+++ src/main/java/com/tsystems/javaschoolshop/aop/Logging.java
@@ -9,19 +9,19 @@ import org.springframework.stereotype.Component;
 public class Logging {
     private Logger logger = Logger.getLogger(Logging.class);
 
-    @Pointcut("@annotation(com.tsystems.javaschoolshop.aop.annotation.Log)")
+    @Pointcut("@annotation(jdk.nashorn.internal.runtime.logging.Logger)")
     public void checkPermission() {
         return ;
     }
 
     @Before("checkPermission()")
     public void logBefore(){
-        logger.info("До...");
+        logger.info("Before...");
         return;
     }
     @After("checkPermission()")
     public void logAfter(){
-        logger.info("После");
+        logger.info("After...");
         return;
     }
 }
