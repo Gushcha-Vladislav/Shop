0e64073bfabc6d401b5c04e72fe161669ad1c8af
gusch <guscha.vladislav1997@yandex.ru>
2018-03-15 22:30:33 +0300

    Log through aop

--- src/main/java/com/tsystems/javaschoolshop/aop/Logging.java
+++ src/main/java/com/tsystems/javaschoolshop/aop/Logging.java
@@ -1,27 +1,41 @@
 package com.tsystems.javaschoolshop.aop;
 
 import org.apache.log4j.Logger;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
 
 @Aspect
-@Component("Logging")
+@Component
 public class Logging {
     private Logger logger = Logger.getLogger(Logging.class);
 
-    @Pointcut("@annotation(jdk.nashorn.internal.runtime.logging.Logger)")
-    public void checkPermission() {
-        return ;
+    @Pointcut("within(com.tsystems.javaschoolshop.service..*) ||" +
+            "within(com.tsystems.javaschoolshop.model..*) ||" +
+            "within(com.tsystems.javaschoolshop.controller..*) ||" +
+            "within(com.tsystems.javaschoolshop.dao..*)")
+    public void witch()  {
     }
 
-    @Before("checkPermission()")
-    public void logBefore(){
-        logger.info("Before...");
+    @Before(value = "witch()")
+    public void logBefore(JoinPoint joinPoint) {
+        logger.info("###### Requested class : {" + joinPoint.getTarget().getClass().getName() + "} ; Method : {" + joinPoint.getSignature().getName() + "} ");
+        Object[] signatureArgs = joinPoint.getArgs();
+        for (Object signatureArg : signatureArgs) {
+            logger.info("###### Arguments: {" + signatureArg.toString() + "} ");
+        }
+    }
+
+    @AfterReturning(pointcut = "witch()")
+    public void logAfter(JoinPoint joinPoint) {
+        logger.info("###### Success return for method :{"+ joinPoint.getSignature().getName()+"}");
         return;
     }
-    @After("checkPermission()")
-    public void logAfter(){
-        logger.info("After...");
+
+    @AfterThrowing(pointcut = "witch()", throwing = "ex")
+    public void exceptionError(Throwable ex) {
+        logger.error("Fatal Error:", ex);
         return;
     }
+
 }
