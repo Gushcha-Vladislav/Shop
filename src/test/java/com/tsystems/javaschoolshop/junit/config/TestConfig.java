548c567796d0b483583bd3c337d3957429137522
gusch <guscha.vladislav1997@yandex.ru>
2018-03-16 21:03:26 +0300

    add test configuration

---
+++ src/test/java/com/tsystems/javaschoolshop/junit/config/TestConfig.java
@@ -0,0 +1,9 @@
+package com.tsystems.javaschoolshop.junit.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.tsystems.javaschoolshop")
+public class TestConfig {
+}
