1350d8728cef7146bb6e029f61979c383479f56f
gusch <guscha.vladislav1997@yandex.ru>
2018-03-19 02:22:31 +0300

    change method add to basket : change state bd

--- src/main/java/com/tsystems/javaschoolshop/service/api/BasketProductService.java
+++ src/main/java/com/tsystems/javaschoolshop/service/api/BasketProductService.java
@@ -4,5 +4,5 @@ import com.tsystems.javaschoolshop.model.dto.BasketProductDto;
 import java.util.List;
 
 public interface BasketProductService {
-    void addToBasket(BasketProductDto basketProductDto, List<BasketProductDto> basket);
+    boolean addToBasket(BasketProductDto basketProductDto, List<BasketProductDto> basket);
 }
--- src/main/java/com/tsystems/javaschoolshop/service/impl/BasketProductServiceImpl.java
+++ src/main/java/com/tsystems/javaschoolshop/service/impl/BasketProductServiceImpl.java
@@ -1,7 +1,10 @@
 package com.tsystems.javaschoolshop.service.impl;
 
+import com.tsystems.javaschoolshop.dao.api.ProductDao;
+import com.tsystems.javaschoolshop.model.Product;
 import com.tsystems.javaschoolshop.model.dto.BasketProductDto;
 import com.tsystems.javaschoolshop.service.api.BasketProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -9,8 +12,35 @@ import java.util.List;
 @Service
 public class BasketProductServiceImpl implements BasketProductService {
 
+
+    private final ProductDao productDao;
+
+    @Autowired
+    public BasketProductServiceImpl(ProductDao productDao) {
+        this.productDao = productDao;
+    }
+
     @Override
-    public void addToBasket(BasketProductDto basketProductDto, List<BasketProductDto> basket) {
-        basket.add(basketProductDto);
+    public boolean addToBasket(BasketProductDto basketProductDto, List<BasketProductDto> basket) {
+        Product product = productDao.findProductById(basketProductDto.getId());
+        boolean isFoundInBag = false;
+        for (BasketProductDto basketProduct : basket) {
+            if (basketProduct.getId() == basketProductDto.getId()) {
+                if(product.getQuantityInStock() <=basketProductDto.getAmount()-basketProduct.getAmount())
+                    return false;
+                basketProduct.setAmount(basketProductDto.getAmount());
+                basketProduct.setPrice(basketProductDto.getPrice());
+                product.setQuantityInStock(product.getQuantityInStock()-basketProductDto.getAmount()+basketProduct.getAmount());
+                isFoundInBag = true;
+                break;
+            }
+        }
+
+        if (!isFoundInBag && product.getQuantityInStock() >=basketProductDto.getAmount()) {
+            basket.add(basketProductDto);
+            product.setQuantityInStock(product.getQuantityInStock()-basketProductDto.getAmount());
+        }else return false;
+        productDao.saveProduct(product);
+        return true;
     }
 }
