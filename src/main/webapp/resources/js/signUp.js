3ea960cebf5a0d794f84ec7c4e2604604a2e5f7a
gusch <guscha.vladislav1997@yandex.ru>
2018-03-21 17:04:39 +0300

    if the fields are not filled then hiding the button + verification of originality of phone and mail

--- src/main/webapp/resources/js/signUp.js
+++ src/main/webapp/resources/js/signUp.js
@@ -1,6 +1,50 @@
 $(document).ready(function () {
-    jQuery(function($){
+    $(function($){
         $("#phone").mask("+7(999) 999-9999");
-        $("#email").mask("[-._a-z0-9]+@(?:[a-z0-9][-a-z0-9]+\\.)+[a-z]{2,6}");
         });
+    $(".required").keyup(
+        function () {
+            if($("#email").val() > 5 && $("#password").val() > 3 && $("#country").val() > 5
+                && $("#city").val() > 5 && $("#street").val() > 5 && $("#house").val() > 5
+                && $("#nameUser").val() > 5){
+                $(".login-button").removeAttr('disabled');
+            } else{
+                $('.login-button').attr('disabled', 'disabled');
+            }
+        }
+    )
+    $("#email").change(
+        function() {
+            $.ajax({
+                url: '/signUp/email',
+                type: 'POST',
+                data: {
+                    "email" : $("#email").val()
+            }
+        }).
+            done(function(recourse) {
+                error(recourse,"phone")
+            });
+        }
+    );
+    $("#email").change(
+        function() {
+            $.ajax({
+                url: '/signUp/phone',
+                type: 'POST',
+                data: {
+                    "phone" : $("#phone").val()
+                }
+            }).
+            done(function(recourse) {
+                error(recourse,"phone")
+            });
+        }
+    );
+    function error(recourse,error){
+        if(recourse === false){
+            $('.login-button').attr('disabled', 'disabled');
+            alert("Error" + error);
+        }
+    }
 });
