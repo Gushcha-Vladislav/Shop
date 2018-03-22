caa66fbd0cc88d34a7577e3b060079c16ffd09b4
gusch <guscha.vladislav1997@yandex.ru>
2018-03-22 12:18:07 +0300

    save order for db

--- src/main/java/com/tsystems/javaschoolshop/dao/impl/OrderDaoImpl.java
+++ src/main/java/com/tsystems/javaschoolshop/dao/impl/OrderDaoImpl.java
@@ -2,8 +2,17 @@ package com.tsystems.javaschoolshop.dao.impl;
 
 import com.tsystems.javaschoolshop.dao.GenericDao;
 import com.tsystems.javaschoolshop.dao.api.OrderDao;
+import com.tsystems.javaschoolshop.model.Order;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public class OrderDaoImpl  extends GenericDao implements OrderDao {
+
+    @Override
+    @Transactional
+    public void saveOrder(Order order) {
+        em.merge(order);
+        em.flush();
+    }
 }
