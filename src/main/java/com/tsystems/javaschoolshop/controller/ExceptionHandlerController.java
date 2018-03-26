c5022123122e677d59f50d4ece5dff9f1d7288c1
gusch <guscha.vladislav1997@yandex.ru>
2018-03-26 21:59:41 +0300

    Authentication

--- src/main/java/com/tsystems/javaschoolshop/controller/ExceptionHandlerController.java
+++ src/main/java/com/tsystems/javaschoolshop/controller/ExceptionHandlerController.java
@@ -2,12 +2,19 @@ package com.tsystems.javaschoolshop.controller;
 
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
 
 @ControllerAdvice
 public class ExceptionHandlerController {
 
     @ExceptionHandler(Exception.class)
-    public String handleException(Exception e) {
-        return "error";
+    public ModelAndView handleException(Exception e) {
+        StringWriter sw = new StringWriter();
+        PrintWriter pw = new PrintWriter(sw);
+        e.printStackTrace(pw);
+        return new ModelAndView("error","exception",sw.toString());
     }
 }
