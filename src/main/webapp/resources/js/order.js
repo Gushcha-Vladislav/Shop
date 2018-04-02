e7afa980e8d74fa19e24f320e4499fd2331049ec
gusch <guscha.vladislav1997@yandex.ru>
2018-04-02 11:45:36 +0300

    add address of order page

--- src/main/webapp/resources/js/order.js
+++ src/main/webapp/resources/js/order.js
@@ -21,4 +21,9 @@ $(document).ready(function(){
     $('.payCard').click(function () {
         window.location='/order/pay?idAddress='+$('#selectForAddress').val()+'&paymentType='+$('#selectPaymentType').val();
     });
+    $('#selectPaymentType').change(function() {
+        if($('#selectForAddress').val() === "newAddress"){
+            window.location="account/formAddress"
+        }
+    });
 });
