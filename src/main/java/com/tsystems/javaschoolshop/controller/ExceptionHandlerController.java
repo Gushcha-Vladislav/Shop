ad809dd8e42446f226a3c560e56a9e11f35f2500
gusch <guscha.vladislav1997@yandex.ru>
2018-04-05 12:51:40 +0300

    add exceprion "Access error, leave the page"

--- src/main/java/com/tsystems/javaschoolshop/controller/ExceptionHandlerController.java
+++ src/main/java/com/tsystems/javaschoolshop/controller/ExceptionHandlerController.java
@@ -1,5 +1,6 @@
 package com.tsystems.javaschoolshop.controller;
 
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.ModelAndView;
@@ -15,6 +16,22 @@ public class ExceptionHandlerController {
         StringWriter sw = new StringWriter();
         PrintWriter pw = new PrintWriter(sw);
         e.printStackTrace(pw);
-        return new ModelAndView("error","message","Unknown error, return to the main page or contact the admin");
+        ModelAndView modelAndView = new ModelAndView("error","message","Unknown error, return to the main page or contact the admin");
+        modelAndView.addObject("exception",sw);
+        return modelAndView;
+    }
+    @ExceptionHandler(AccessDeniedException.class)
+    public ModelAndView exceptionAccess(Exception e) {
+        StringWriter sw = stackTraceToString(e);
+        ModelAndView modelAndView = new ModelAndView("error","message","Access error, leave the page");
+        modelAndView.addObject("exception",sw);
+        return modelAndView;
+    }
+
+    private StringWriter stackTraceToString(Exception e){
+        StringWriter sw = new StringWriter();
+        PrintWriter pw = new PrintWriter(sw);
+        e.printStackTrace(pw);
+        return sw;
     }
 }
