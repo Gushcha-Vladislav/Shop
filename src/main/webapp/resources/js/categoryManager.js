faf021e42ec707c9626edb9c427f011895736653
gusch <guscha.vladislav1997@yandex.ru>
2018-04-05 12:55:26 +0300

    category manager js

---
+++ src/main/webapp/resources/js/categoryManager.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+    $(".categoryChange").click(function () {
+        var $input = $(this).parent().find('div.id.param');
+        var $parent = $(this).parent().parent().find('div.id.param')[0];
+        $("#nameCategory").val($(this).text());
+        $("#id").val($input.html());
+        $("#idParent").val($parent.val());
+    });
+    $("#idParent").change(function() {
+        var $input = $(this).parent().find('div.id.param');
+        $.ajax({
+            url: '/admin/categories/'+ $input.html(),
+            type: 'GET'
+        }).done( function (recourse) {
+            if(!recourse) $("#messageParent").html("This category is at the top of the hierarchy.");
+            else alert("Done");
+        });
+    });
+    $(".status").click(function() {
+        var $input = $(this).parent().find('div.id.param');
+        $.ajax({
+            url: '/admin/categories/status/'+ $input.html(),
+            type: 'GET'
+        }).done( function (recourse) {
+            window.location="/admin/categories";
+        });
+    });
+});
