82f6ace228daf5be45406a75eb071fb08bc6d5ed
gusch <guscha.vladislav1997@yandex.ru>
2018-04-07 06:52:48 +0300

    class for convert image. For adding image in pdf

---
+++ src/main/java/com/tsystems/javaschoolshop/util/pdf/ByteArrayConverterUtil.java
@@ -0,0 +1,41 @@
+package com.tsystems.javaschoolshop.util.pdf;
+
+/**
+ * This class helps you to convert byte arrays. For example, from primitive byte[]
+ * to boxed Byte[] and vice versa
+ */
+public class ByteArrayConverterUtil {
+
+    /**
+     * Empty constructor
+     */
+    private ByteArrayConverterUtil() {
+
+    }
+
+    /**
+     * This method converts Byte[] to byte[]
+     * @param bytes - method gets boxed Byte array
+     * @return converted primitive byte array
+     */
+    public static byte[] convertBytes(Byte[] bytes) {
+        byte[] resultBytes = new byte[bytes.length];
+        for (int i = 0; i < bytes.length; i++) {
+            resultBytes[i] = bytes[i];
+        }
+        return resultBytes;
+    }
+
+    /**
+     * This method converts byte[] to Byte[]
+     * @param bytes - method gets primitive byte array
+     * @return converted boxed Byte array
+     */
+    public static Byte[] convertBytes(byte[] bytes) {
+        Byte[] resultBytes = new Byte[bytes.length];
+        for (int i = 0; i < bytes.length; i++) {
+            resultBytes[i] = bytes[i];
+        }
+        return resultBytes;
+    }
+}
