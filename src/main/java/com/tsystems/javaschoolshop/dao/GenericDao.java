b9039ac873850404255aafc2a1ce6d4017b6ee17
gusch <guscha.vladislav1997@yandex.ru>
2018-04-07 19:44:06 +0300

    java doc for genericDao

--- src/main/java/com/tsystems/javaschoolshop/dao/GenericDao.java
+++ src/main/java/com/tsystems/javaschoolshop/dao/GenericDao.java
@@ -3,8 +3,17 @@ package com.tsystems.javaschoolshop.dao;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
+/**
+ * generic for all DAO, have entity manager
+ */
 public abstract class GenericDao {
 
+    /**
+     * It's an exemplar created by EntityManagerFactory we created and configured in DatabaseConfig class.
+     * EntityManager provide us special API for working with database.
+     * In particular, we can create different HQL queries. After executing that queries will give us
+     * all necessary data.
+     */
     @PersistenceContext
     protected EntityManager em;
 }
