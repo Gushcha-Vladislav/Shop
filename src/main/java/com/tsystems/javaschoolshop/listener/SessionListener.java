0d715982f8ec0d0756559bf0c06abffc7f3ae446
gusch <guscha.vladislav1997@yandex.ru>
2018-04-07 19:44:29 +0300

    java doc for  sessionListener

--- src/main/java/com/tsystems/javaschoolshop/listener/SessionListener.java
+++ src/main/java/com/tsystems/javaschoolshop/listener/SessionListener.java
@@ -15,19 +15,38 @@ import javax.servlet.http.HttpSessionEvent;
 import javax.servlet.http.HttpSessionListener;
 
 
+/**
+ * Session listener. When session is timed out it clears user bag
+ * and put all items in database again.
+ */
 public class SessionListener implements ServletContextListener, HttpSessionListener {
 
+    /**
+     * Injected product service which allows us to work with database products.
+     */
     @Autowired
     private ProductService productService;
 
+    /**
+     * Injected basket bean which allows us to work with basket products.
+     */
     @Autowired
     private BasketBean basketBean;
 
+    /**
+     * Event when user's session is created.
+     */
     @Override
     public void sessionCreated(HttpSessionEvent httpSessionEvent) {
         httpSessionEvent.getSession().setMaxInactiveInterval(60*60*2);
     }
 
+    /**
+     * Event when user's session must be destroyed.
+     * It also happens when user successfully becomes authorised.
+     * And our goal is preventing bag clearing in this case(SPRING_SECURITY_SAVED_REQUEST).
+     * In other cases we should clear bag and put items back to the database.
+     */
     @Override
     @Transactional
     public void sessionDestroyed(HttpSessionEvent httpSessionEvent) {
@@ -41,6 +60,10 @@ public class SessionListener implements ServletContextListener, HttpSessionListe
         basketBean.getBasket().clear();
     }
 
+    /**
+     * Spring can't inject any bean into listeners by standard tools.
+     * And we should do it to use product service.
+     */
     @Override
     public void contextInitialized(ServletContextEvent sce) {
         SpringBeanAutowiringSupport.processInjectionBasedOnCurrentContext(this);
