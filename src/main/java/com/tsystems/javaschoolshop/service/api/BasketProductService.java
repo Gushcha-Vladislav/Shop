ff47e450e15023d82064787028294b57a29e4657
gusch <guscha.vladislav1997@yandex.ru>
2018-04-07 06:50:51 +0300

    add java doc for class BasketProductService

--- src/main/java/com/tsystems/javaschoolshop/service/api/BasketProductService.java
+++ src/main/java/com/tsystems/javaschoolshop/service/api/BasketProductService.java
@@ -4,13 +4,67 @@ import com.tsystems.javaschoolshop.model.Product;
 import com.tsystems.javaschoolshop.model.dto.BasketProductDto;
 import java.util.List;
 
+/**
+ * Interface provide us API we can use to manipulate our Basket products.
+ */
 public interface BasketProductService {
+
+    /**
+     * Method add product to the basket.
+     * @param basketProductDto - the product that will be added.
+     * @param basket existing products in basket
+     */
     boolean addToBasket(BasketProductDto basketProductDto, List<BasketProductDto> basket);
+
+    /**
+     * Method delete product from the basket by product id.
+     * @param id  - id of the product that will be deleted.
+     * @param basket - existing products in basket.
+     */
     boolean deleteFromBasketById(int id, List<BasketProductDto> basket);
-    Integer countProductsInBagById(int id, List<BasketProductDto> basket);
+
+    /**
+     * Method calculates count products of the user's basket by product id.
+     * @param id  - id of the product that will be deleted.
+     * @param basket - given list of products(BasketProductDto)
+     * @return total price in String format.
+     */
+    Integer countProductsInBasketById(int id, List<BasketProductDto> basket);
+    /**
+     * Method calculates total price of the user's basket.
+     * @param basket - given list of products(BasketProductDto)
+     * @return total price in String format.
+     */
     int totalPrice(List<BasketProductDto> basket);
-    Integer countProductsInBag(List<BasketProductDto> basket);
+
+    /**
+     * Method calculates count products of the user's basket.
+     * @param basket - given list of products(BasketProductDto)
+     * @return total price in String format.
+     */
+    Integer countProductsInBasket(List<BasketProductDto> basket);
+
+    /**
+     * Method delete all product from the basket.
+     * @param basket - existing products in basket.
+     */
     List<BasketProductDto> deleteFromBasket(List<BasketProductDto> basket);
-    BasketProductDto createBagProductFromProduct(Product product);
+
+    /**
+     * During the application execution we should have a possibility
+     * to convert our Product {@link Product}
+     * to basket product object {@link BasketProductDto}.
+     * @param product that will be converted
+     * @return converted basketProductDto
+     */
+    BasketProductDto createBasketProductFromProduct(Product product);
+
+    /**
+     * During the application execution we should have a possibility
+     * to convert our basket product(basketProductDto as you remember {@link BasketProductDto})
+     * to simple Product object {@link Product}.
+     * @param basketProductDto one.
+     * @return converted Product object.
+     */
     Product convertBasketProductDtoToProduct(BasketProductDto basketProductDto);
 }
--- src/main/java/com/tsystems/javaschoolshop/service/impl/BasketProductServiceImpl.java
+++ src/main/java/com/tsystems/javaschoolshop/service/impl/BasketProductServiceImpl.java
@@ -12,22 +12,37 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Basket product service. It is used to bag manipulations.
+ */
 @Service
 public class BasketProductServiceImpl implements BasketProductService {
 
-
+    /**
+     * Injected by spring productDao bean
+     */
     private final ProductDao productDao;
 
+    /**
+     * Injecting constructor
+     * @param productDao that must be injected
+     */
     @Autowired
     public BasketProductServiceImpl(ProductDao productDao) {
         this.productDao = productDao;
     }
 
+    /**
+     * Method add product to the bag.
+     *
+     * @param basketProductDto  - the product that will be added.
+     * @param basket existing products in basket
+     */
     @Override
     @Transactional
     public boolean addToBasket(BasketProductDto basketProductDto, List<BasketProductDto> basket) {
         Product product = productDao.findProductById(basketProductDto.getId());
-        boolean isFoundInBag = false;
+        boolean isFoundInbasket = false;
         for (BasketProductDto basketProduct : basket) {
             if (basketProduct.getId() == basketProductDto.getId()) {
                 if (product.getQuantityInStock() <= basketProductDto.getAmount() - basketProduct.getAmount())
@@ -36,12 +51,12 @@ public class BasketProductServiceImpl implements BasketProductService {
                 basketProduct.setPrice(basketProductDto.getPrice());
                 product.setQuantityInStock(product.getQuantityInStock() - basketProductDto.getAmount() + basketProduct.getAmount());
                 product.getStatisticTopProduct().setAmount(product.getStatisticTopProduct().getAmount()+ basketProductDto.getAmount() - basketProduct.getAmount());
-                isFoundInBag = true;
+                isFoundInbasket = true;
                 break;
             }
         }
 
-        if (!isFoundInBag)
+        if (!isFoundInbasket)
             if(product.getQuantityInStock() >= basketProductDto.getAmount()) {
                 basket.add(basketProductDto);
                 if(product.getStatisticTopProduct()==null) product.setStatisticTopProduct(new StatisticTopProduct(product,0));
@@ -52,6 +67,12 @@ public class BasketProductServiceImpl implements BasketProductService {
         return true;
     }
 
+    /**
+     * Method delete product from the basket by product id.
+     *
+     * @param id  - id of the product that will be deleted.
+     * @param basket - existing products in basket.
+     */
     @Override
     @Transactional
     public boolean deleteFromBasketById(int id, List<BasketProductDto> basket) {
@@ -69,8 +90,15 @@ public class BasketProductServiceImpl implements BasketProductService {
         return true;
     }
 
+    /**
+     * Method calculates count products of the user's basket by product id.
+     *
+     * @param id  - id of the product that will be deleted.
+     * @param basket - given list of products(BasketProductDto)
+     * @return total price in String format.
+     */
     @Override
-    public Integer countProductsInBagById(int id, List<BasketProductDto> basket) {
+    public Integer countProductsInBasketById(int id, List<BasketProductDto> basket) {
         for (BasketProductDto product : basket) {
             if (product.getId() == id) {
                 return product.getAmount();
@@ -79,6 +107,12 @@ public class BasketProductServiceImpl implements BasketProductService {
         return 0;
     }
 
+    /**
+     * Method calculates total price of the user's basket.
+     *
+     * @param basket - given list of products(BagProductDto)
+     * @return total price in String format.
+     */
     @Override
     public int totalPrice(List<BasketProductDto> basket) {
         int totalPrice = 0;
@@ -88,8 +122,14 @@ public class BasketProductServiceImpl implements BasketProductService {
         return totalPrice;
     }
 
+    /**
+     * Method calculates count products of the user's basket.
+     *
+     * @param basket - given list of products(BasketProductDto)
+     * @return total price in String format.
+     */
     @Override
-    public Integer countProductsInBag(List<BasketProductDto> basket) {
+    public Integer countProductsInBasket(List<BasketProductDto> basket) {
         int count = 0;
         for (BasketProductDto product : basket) {
             count += product.getAmount();
@@ -97,6 +137,11 @@ public class BasketProductServiceImpl implements BasketProductService {
         return count;
     }
 
+    /**
+     * Method delete all product from the basket.
+     *
+     * @param basket - existing products in basket.
+     */
     @Override
     @Transactional
     public List<BasketProductDto> deleteFromBasket(List<BasketProductDto> basket) {
@@ -109,12 +154,29 @@ public class BasketProductServiceImpl implements BasketProductService {
 
         return new ArrayList<>();
     }
+
+    /**
+     * During the application execution we should have a possibility
+     *
+     * to convert our Product {@link Product}
+     * to basket product object {@link BasketProductDto}.
+     * @param product that will be converted
+     * @return converted basketProductDto
+     */
     @Override
-    public  BasketProductDto createBagProductFromProduct(Product product) {
+    public  BasketProductDto createBasketProductFromProduct(Product product) {
         return new BasketProductDto (product.getId(),product.getNameProduct(), 1,
                 product.getPrice(), product.getImage());
     }
 
+    /**
+     * During the application execution we should have a possibility
+     *
+     * to convert our basket product(basketProductDto as you remember {@link BasketProductDto})
+     * to simple Product object {@link Product}.
+     * @param basketProductDto one.
+     * @return converted Product object.
+     */
     @Override
     public Product convertBasketProductDtoToProduct(BasketProductDto basketProductDto) {
         if (basketProductDto == null) return null;
