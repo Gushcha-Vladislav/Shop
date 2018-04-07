65e7fbae84354cf23354b428b78355f9b85ae7a3
gusch <guscha.vladislav1997@yandex.ru>
2018-04-07 19:51:18 +0300

    java doc for basket bean

--- src/main/java/com/tsystems/javaschoolshop/session/BasketBean.java
+++ src/main/java/com/tsystems/javaschoolshop/session/BasketBean.java
@@ -8,16 +8,27 @@ import org.springframework.stereotype.Component;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Storage Bean
+ */
 @Component("basketBean")
 @Scope(value = "session", proxyMode = ScopedProxyMode.TARGET_CLASS)
 public class BasketBean {
 
     private List<BasketProductDto> basket = new ArrayList<>();
 
+    /**
+     * Getter of the basket
+     * @return BasketProductDto list
+     */
     public List<BasketProductDto> getBasket() {
         return basket;
     }
 
+    /**
+     * Setter of the basket
+     * @param basket - basket list
+     */
     public void setBasket(List<BasketProductDto> basket) {
         this.basket = basket;
     }
