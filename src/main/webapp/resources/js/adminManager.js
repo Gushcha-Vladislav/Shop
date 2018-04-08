8a23bac87c000da2c72d3c97665304bc1b6119dc
gusch <guscha.vladislav1997@yandex.ru>
2018-04-08 12:52:45 +0300

    add correct url

--- src/main/webapp/resources/js/adminManager.js
+++ src/main/webapp/resources/js/adminManager.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     $('.deleteAdmin').click(function () {
         var $input = $(this).parent().find('div.param');
         $.ajax({
-            url: 'superAdmin/manager/delete/'+$input.html(),
+            url: '/superAdmin/manager/delete/'+$input.html(),
             type: 'POST'
         }).done( function (response) {
             $('#adminList').html(response);
