8b456a66d23ee5e73a79bc1dd84d20a4506cfa57
gusch <guscha.vladislav1997@yandex.ru>
2018-04-08 12:36:02 +0300

    change max size upload image

--- src/main/java/com/tsystems/javaschoolshop/config/WebConfig.java
+++ src/main/java/com/tsystems/javaschoolshop/config/WebConfig.java
@@ -82,7 +82,7 @@ public class WebConfig extends WebMvcConfigurerAdapter {
     public CommonsMultipartResolver multipartResolver() {
         CommonsMultipartResolver resolver = new CommonsMultipartResolver();
         resolver.setDefaultEncoding("UTF-8");
-        resolver.setMaxUploadSize(1024*1024*40);
+        resolver.setMaxUploadSize(1024*1024);
         return resolver;
     }
 
